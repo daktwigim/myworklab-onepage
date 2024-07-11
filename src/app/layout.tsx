079ddeafc4be325091aa8f2be65d0e283a671e90
Park Hansu <daktwigim@gmail.com>
2024-07-11 17:59:16 +0900

    version 1

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "마이워크랩",
+  description: "사람들에게 하고 싶은 일을 찾아주고 그들이 즐겁고 의미 있게 일하는 세상을 만듭니다.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://myworklab.com" />
+        <meta property="og:title" content="마이워크랩" />
+        <meta property="og:image" content="/assets/thumbnail.png" />
+        <meta
+          property="og:description"
+          content="사람들에게 하고 싶은 일을 찾아주고 그들이 즐겁고 의미 있게 일하는 세상을 만듭니다."
+        />
+        <meta property="og:site_name" content="마이워크랩" />
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </head>
+      <body className={inter.className}>{children}</body>
+      <footer className="p-2 flex justify-center items-center">
+        <span className="text-white">Copyright 2024. 유덕수. All rights reserved.</span>
+      </footer>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,169 @@
+"use client";
+
+import Image from "next/image";
+import { ReactEventHandler, useEffect, useRef, useState } from "react";
+
+export default function Home() {
+  const [videoLoaded, setVideoLoaded] = useState<boolean>(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  function checkVideoLoaded(video: HTMLVideoElement) {
+    console.log("video loaded");
+    setVideoLoaded(true);
+    video.play();
+  }
+
+  useEffect(() => {
+    console.log("effected");
+    const video = videoRef.current;
+    console.log(video);
+    if (video) {
+      checkVideoLoaded(video);
+    }
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      {/* Backgroud Image and Video */}
+      <div className="fixed top-0 left-0 w-full h-full -z-10">
+        {/* Background darkter fillter */}
+        <div className="absolute top-0 left-0 w-full h-full bg-zinc-950/80 z-50"></div>
+        {/* Background Video */}
+        <video
+          className="w-full h-full object-cover"
+          muted
+          autoPlay
+          loop
+          ref={videoRef}
+        >
+          <source
+            src="/video/background.mp4"
+            type="video/mp4"
+          />
+        </video>
+        {/* Background Image Thumbnail */}
+        {!videoLoaded && (
+          <Image
+            className="absolute top-0 left-0"
+            src="/img/background.png"
+            fill
+            alt="배경 이미지"
+          />
+        )}
+      </div>
+
+      {/* Contents */}
+      <div className="container lg:max-w-4xl">
+        <Image
+          className="mb-2"
+          src="/img/logo.svg"
+          alt="마이워크랩"
+          width={380}
+          height={50}
+        />
+        <h2 className="text-lg lg:text-2xl mb-8">
+          사람들에게 하고 싶은 일을 찾아주고,
+          <br /> 그들이 경제적 자유를 바탕으로 즐겁고 의미 있게 일하는 세상을 만듭니다
+        </h2>
+        <ul className="flex gap-4 flex-col lg:flex-row">
+          <li className="flex-1 border rounded-lg p-3 cursor-pointer">
+            <a
+              className="flex-1 flex lg:flex-col justify-center items-center"
+              href="/"
+            >
+              <Image
+                className="lg:mb-2 mr-4 lg:mr-0 w-9 lg:w-12"
+                src={require("@/assets/svg/myworklab.svg")}
+                width={48}
+                height={48}
+                alt="마이워크랩 소개"
+              ></Image>
+              <div className="flex-1 flex justify-between">
+                <span className="flex-1 lg:mr-1">마이워크랩 소개</span>
+                <Image
+                  className="justify-self-end"
+                  src={require("@/assets/svg/link.svg")}
+                  width={18}
+                  height={18}
+                  alt="바로가기"
+                />
+              </div>
+            </a>
+          </li>
+          <li className="flex-1 border rounded-lg p-3 cursor-pointer">
+            <a
+              className="flex lg:flex-col justify-center items-center"
+              href="/"
+            >
+              <Image
+                className="lg:mb-2 mr-4 lg:mr-0 w-9 lg:w-12"
+                src={require("@/assets/svg/lecture.svg")}
+                width={48}
+                height={48}
+                alt="교육 프로그램"
+              ></Image>
+              <div className="flex-1 flex justify-between">
+                <span className="flex-1 lg:mr-1">교육 프로그램</span>
+                <Image
+                  className="justify-self-end"
+                  src={require("@/assets/svg/link.svg")}
+                  width={18}
+                  height={18}
+                  alt="바로가기"
+                />
+              </div>
+            </a>
+          </li>
+          <li className="flex-1 border rounded-lg p-3 cursor-pointer">
+            <a
+              className="flex lg:flex-col justify-center items-center"
+              href="/"
+            >
+              <Image
+                className="lg:mb-2 mr-4 lg:mr-0 w-9 lg:w-12"
+                src={require("@/assets/svg/newsletter.svg")}
+                width={48}
+                height={48}
+                alt="뉴스레터"
+              ></Image>
+              <div className="flex-1 flex justify-between">
+                <span className="flex-1 lg:mr-1">뉴스레터</span>
+                <Image
+                  className="justify-self-end"
+                  src={require("@/assets/svg/link.svg")}
+                  width={18}
+                  height={18}
+                  alt="바로가기"
+                />
+              </div>
+            </a>
+          </li>
+          <li className="flex-1 border rounded-lg p-3 cursor-pointer">
+            <a
+              className="flex lg:flex-col justify-center items-center"
+              href="/"
+            >
+              <Image
+                className="lg:mb-2 mr-4 lg:mr-0 w-9 lg:w-12"
+                src={require("@/assets/svg/instagram.svg")}
+                width={48}
+                height={48}
+                alt="인스타그램"
+              ></Image>
+              <div className="flex-1 flex justify-between">
+                <span className="flex-1 lg:mr-1">인스타그램</span>
+                <Image
+                  className="justify-self-end"
+                  src={require("@/assets/svg/link.svg")}
+                  width={18}
+                  height={18}
+                  alt="바로가기"
+                />
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
